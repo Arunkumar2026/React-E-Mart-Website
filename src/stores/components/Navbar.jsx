bdb57ad1cf79f26c7d953abdecd869ebfb679007
Arun Kumar <akulakumar453@gmail.com>
2026-02-07 12:06:20 +0530

    e-mart

---
+++ src/stores/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navSection">
+        <div className="title">
+            <h2>E-Mart</h2>
+        </div>
+        <div className="search">
+            <input type="text" placeholder='Search...' />
+        </div>
+        <div className="user">
+            <div className="user-detail">
+                SignIN/SignUp 
+            </div>
+            <div className="cart">
+                Cart
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
