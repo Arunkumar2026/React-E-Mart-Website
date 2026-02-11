da4d0419f58bda4a6ea31879b32101a110dd5234
Arun Kumar <akulakumar453@gmail.com>
2026-02-11 14:47:31 +0530

    This is speakers file

---
+++ src/stores/components/Speakers.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {speakerData} from '../data/speaker'
+
+const Speakers = () => {
+    const firstFiveImages = speakerData.slice(0,5)
+  return (
+    <>
+        <div className="proTitle">
+            <h2>Speakers</h2>
+        </div>
+        <div className="proSection">
+            {
+                firstFiveImages.map((item)=>{
+                    return(
+                        <div className="imgBox">
+                            <img src={item.image} alt="" className="proImage" />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </>
+  )
+}
+
+export default Speakers
