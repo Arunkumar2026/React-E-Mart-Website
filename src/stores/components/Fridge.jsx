5c60c4bd724c0b1ca100ffd1c95a883bce3852a7
Arun Kumar <akulakumar453@gmail.com>
2026-02-11 14:47:06 +0530

    This is fridge file

---
+++ src/stores/components/Fridge.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {fridgeData} from '../data/fridge'
+
+const Fridge = () => {
+    const firstFiveImages = fridgeData.slice(0,5)
+  return (
+    <>
+        <div className="proTitle">
+            <h2>Fridge</h2>
+        </div>
+        <div className="proSection">
+            {
+                firstFiveImages.map((item)=>{
+                    return(
+                        <div className="imgBox">
+                            <img src={item.image} alt="" className="proImage" />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </>
+  )
+}
+
+export default Fridge
